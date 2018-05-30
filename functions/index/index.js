53d1d02073c1cae3d6b8f1242abe8277e26bf318
Patrick Marx <patrick.k.marx@gmail.com>
2018-05-29 20:49:30 -0700

    POC w/ text

---
+++ functions/index/index.js
@@ -0,0 +1,33 @@
+console.log("Loading function");
+
+var request = require('request');
+var AWS = require("aws-sdk");
+
+var surfableStates = ['Fair', 'Fair-Good', 'Good']
+ 
+
+exports.handle = function(event, context) {
+  var sns = new AWS.SNS();
+  var params = {
+      Message: "Shaka braaaaaah", 
+      Subject: "Git er done",
+      TopicArn: "arn:aws:sns:us-east-1:606748693934:surfs-lookin-good"
+  };
+
+  request('http://api.spitcast.com/api/spot/forecast/120', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    console.log(body);
+
+    var tomorrowForecast = body[0]; //TODO
+
+    if (true){//surfableStates.includes(tomorrowForecast.shape_full)) {
+      console.log('lookin good!');
+      sns.publish(params, context.done);
+    } else {
+      console.log('not today buddy');
+    }
+
+  });
+};
+
+
